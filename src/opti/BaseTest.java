785e00aa7595158f69a0e240f683041fe6a0868c
xiezhiping <1529464401@qq.com>
2019-03-15 11:35:22 +0800

    装箱拆箱研究

---
+++ src/opti/BaseTest.java
@@ -0,0 +1,20 @@
+package opti;
+
+public class BaseTest {
+	public static void main(String[] args) {
+		Integer a = 1;
+		Integer b = 2;
+		Integer c = 3;
+		Integer d = 3;
+		Integer e = 321;
+		Integer f = 321;
+		Long g = 3L;
+		System.out.println(c == d);
+		System.out.println(e == f);
+		System.out.println(c == (a + b));
+		System.out.println(c.equals(a+b));
+		System.out.println(g == (a+b));  // 包装类的“==”运算在不遇到算术运算的情况下不会自动拆箱
+		System.out.println(g.equals(a+b)); // 以及他们的equals方法不处理数据转型的关系
+		
+	}
+}
